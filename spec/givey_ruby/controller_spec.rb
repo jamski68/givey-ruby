025bf12dcb5a9a7ca00d272adad646677995501c
Philip Poots <philip.poots@gmail.com>
2013-07-19 20:23:12 +0100

    Specify key and secret

--- spec/givey_ruby/controller_spec.rb
+++ spec/givey_ruby/controller_spec.rb
@@ -9,7 +9,7 @@ describe GiveyRuby::Controller do
     end
 
     GiveyRuby.configure do |config|
-      config.client({token_file: "#{SPEC_ROOT}/tmp/givey_token_file"})
+      config.client({token_file: "#{SPEC_ROOT}/tmp/givey_token_file", consumer_key: "key", consumer_secret: "secret"})
     end
   end
 
